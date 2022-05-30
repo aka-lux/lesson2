70de3e860326bf7790e2b6fb590e9739f0e7826a
u1 <u1@u1.com>
2022-05-30 22:28:27 +0300

    Initial commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,7 +5,20 @@ public class Calculator {
     * или 0 если операция не возможна или указан неправильный оператор
     */
     public static int сalculator(int firstNumber, char operation, int secondNumber){
-        return 0;
+        if(operation == '*'){
+            return firstNumber*secondNumber;
+        }
+        if(operation == '+'){
+            return firstNumber+secondNumber;
+        }
+        if(operation == '-'){
+            return firstNumber-secondNumber;
+        }
+        if(operation == '/' && secondNumber !=0 ){
+            return firstNumber/secondNumber;
+        }else{
+            return 0;
+        }
     }
 
 
