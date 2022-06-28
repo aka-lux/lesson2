b717a27b989d87a0af3821b1a36c9f9e25ca3043
dmitrenkovalerij <dmitrenkovalerij86@gmail.com>
2022-06-28 18:32:49 +0300

    test1

--- src/main/java/NewFunctions/mathModFunctions.java
+++ src/main/java/NewFunctions/mathModFunctions.java
@@ -6,9 +6,10 @@ import java.io.InputStreamReader;
 
 public class mathModFunctions {
     public static void main(String[] args) throws IOException {
+        task46();
 //        task41();
 //        task43();
-        task44();
+//        task45();
 //        for (int i = 0; i < 5; i++) {
 //            System.out.println(i);
 //            task42();
@@ -30,45 +31,67 @@ public class mathModFunctions {
     public static void task42() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name = reader.readLine();
-        if (name.length() >2) {
+        if (name.length() > 2) {
             System.out.println(name.charAt(0) + " " + name.charAt(1) + " " + name.charAt(2));
-        }
-        else {
+        } else {
             System.out.println(
                     "Incorrect value"
             );
         }
-}
-//    Задание 4.3
+    }
+
+    //    Задание 4.3
 //    Напишите класс, который принимает с клавиатуры целое положительное двузначное число и выводит на экран его цифры, разделенные
 //    знаком «пробел», но в обратном порядке. То есть для числа 45 будет
 //    выведено на экран 5 4.
-public static void task43() throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String name = reader.readLine();
-    System.out.println(name.charAt(1) + " " + name.charAt(0));
+    public static void task43() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        System.out.println(name.charAt(1) + " " + name.charAt(0));
     }
 
-//    Задание 4.4
+    //    Задание 4.4
 //    Напишите класс, который принимает с клавиатуры целое положительное трехзначное число и выводит на экран его цифры, разделенные
 //    знаком «пробел», но в обратном порядке. То есть для числа 415 будет
 //    выведено на экран 5 1 4.
-//
-//    Задание 4.5
+    public static void task44() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        if (name.length() > 2) {
+            System.out.println(name.charAt(2) + " " + name.charAt(1) + " " + name.charAt(0));
+        } else {
+            System.out.println(
+                    "Incorrect value"
+            );
+        }
+    }
+
+    //    Задание 4.5
 //    Напишите класс, который принимает с клавиатуры целое положительное двузначное число и выводит его на экран
 //        в «полном виде»: например, для числа 34 это будет выглядеть как 30+4.
-public static void task44() throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String name = reader.readLine();
-    int digit1 = Integer.parseInt(
-            String.valueOf(
-            name.charAt(0)))*10;
-    System.out.println(digit1 + " " + name.charAt(1));
-}
-//    Задание 4.6
+    public static void task45() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int digit1 = Integer.parseInt(
+                String.valueOf(
+                        name.charAt(0))) * 10;
+        System.out.println(digit1 + " " + name.charAt(1));
+    }
+
+    //    Задание 4.6
 //    Напишите класс, который принимает с клавиатуры целое положительное трехзначное число и выводит его на экран
 //        в «полном виде»: например, для числа 364 это будет выглядеть как 300+60+4.
-//
+    public static void task46() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int digit1 = Integer.parseInt(
+                String.valueOf(
+                        name.charAt(0))) * 100;
+        int digit2 = Integer.parseInt(
+                String.valueOf(
+                        name.charAt(1))) * 10;
+        System.out.println(digit1 + " " + digit2 + " " + name.charAt(2));
+    }
 //    Задание 4.7
 //    Напишите класс, который принимает с клавиатуры целое положительное двузначное число.
 //    Затем следует построить новое значение, составленное из цифр числа, введенного с клавиатуры, но в обратном
